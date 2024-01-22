364edd30e3a9a2c838078e16400fc9dc52e6e6a0
Will MacLean <maclean.will@gmail.com>
2024-01-22 14:54:33 -0600

    inventory

--- client/src/managers/bikeManager.js
+++ client/src/managers/bikeManager.js
@@ -1,3 +1,4 @@
+const apiUrl = "http://localhost:8088/bikes";
 
 export const getBikes = () => {
   return fetch(
@@ -9,11 +10,13 @@ export const getBikeById = (id) => {
   return fetch(
     `${apiUrl}/${id}?_embed=workOrders&_expand=user&_expand=bikeType`
   ).then((res) => res.json())
-
+}
 
 export const getBikesInShopCount = () => {
   //add implementation here...
+  return fetch(
+    `http://localhost:8088/workOrders/?{dateCompleted=""}`
+  ).then((res) => res.json())
 };
 
 
-}
